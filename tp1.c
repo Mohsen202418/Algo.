44d121660e0a507918162c44aa2fd9c64acd4885
Mohsen202418 <prooussama2022@gmail.com>
2024-10-29 12:16:31 +0100

    tp1.c

--- tp1.c
+++ tp1.c
@@ -1,6 +1,7 @@
+
 #include <stdio.h>
 
-int main() {
+int main() { printf("Exercise 1 :\n");
     char M[5][5] = {
         {'1', '2', '3', '4', '5'},
         {'7', 'a', 'c', '8', 'd'},
@@ -32,4 +33,36 @@ int main() {
         }
         printf("\n");
     }
+    
+    
+   printf("Exercise 2 :\n");
+    
+{
+     int A[4][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+     int T;
+     int i,j;
+     printf("tableau inital:\n");
+     for (i=0;i<4;i++){
+        for(j=0;j<4;j++){
+                printf("%d\t",A[i][j]);
+        }
+        printf("\n");
+     }
+     for (i=0;i<3;i++){
+        for(j=i+1;j<4;j++){
+            T=A[i][j];
+            A[i][j]=A[j][i];
+            A[j][i]=T;
+        }
+     }
+     printf("tableau echange:\n");
+     for (i=0;i<4;i++){
+        for(j=0;j<4;j++){
+                printf("%d\t",A[i][j]);
+        }
+               printf("\n");
+     }
+    return 0;
 }
+}
+    
